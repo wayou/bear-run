997fccdf11eb3654cd70800eed3d1c0ee8b87e6b
wayou <liuwayong@gmail.com>
2015-01-31 13:40:55 +0800

    upgrade change log

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
  * bear run
  * this is a html5 web game built with phaser
  * and inspired by chorme offline hidden game t-rex runner
- * v0.1.1
+ * v0.1.2
  * for more info pls head to https://github.com/wayou/bear-run
  *
  *credits:
@@ -11,6 +11,10 @@
  *- jump snd http://www.freesound.org/people/LloydEvans09/sounds/187024/
  *
  * convert wav audio to mp3 http://media.io/
+ *
+ * CHANGELOG
+ * v0.1.2
+ * - fix obstacle recyling and memery leask on mobile
  * v0.1.1
  * - add sound for game over
  */
@@ -26,7 +30,7 @@ game.global = {
     isOver: false,
     status: 0, //0 not started|1 started|2 game over
     level: 1,
-    maxRatio:3,
+    maxRatio: 3,
     ratio: 1 //determie the scrolling speed and the frequency of the obstacles,increase with the time elapsed
 }
 
