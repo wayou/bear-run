3b5513c5a1424b31a0fb51f881bcfca9ba8492f8
Wayou Liu <liuwayong@gmail.com>
2015-02-05 01:56:18 +0800

    remove no using files

--- src/js/main.js
+++ src/js/main.js
@@ -82,12 +82,9 @@ game.global = {
     MAX_SPEED: -460
 };
 
-window.Utils = require('./utils');
-
 game.state.add('Boot', require('./states/boot'));
 game.state.add('Preloader', require('./states/preloader'));
 game.state.add('Menu', require('./states/menu'));
 game.state.add('Game', require('./states/game'));
-game.state.add('Gameover', require('./states/gameover'));
 
 game.state.start('Boot');
