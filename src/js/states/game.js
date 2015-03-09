ef4198949cf306bd875739f53cb5b97362a63a85
wayou <liuwayong@gmail.com>
2015-03-09 20:08:50 +0800

    add btn for leba

--- src/js/states/game.js
+++ src/js/states/game.js
@@ -287,7 +287,11 @@ Game.prototype = {
 
         var goBtn = document.querySelector('#button-go');
         goBtn.style.left = (parseInt(gameCanvas.style.marginLeft) || 0) + parseInt(gameCanvas.style.width) / 2 - 84 + 'px';
-        goBtn.style.top = (parseInt(gameCanvas.style.marginTop) || 0) + parseInt(gameCanvas.style.height) - 80 + 'px';
+        goBtn.style.top = (parseInt(gameCanvas.style.marginTop) || 0) + parseInt(gameCanvas.style.height) - 60 + 'px';
+
+        var moreBtn = document.querySelector('#button-more');
+        moreBtn.style.left = (parseInt(gameCanvas.style.marginLeft) || 0) + parseInt(gameCanvas.style.width) / 2 - 84 + 'px';
+        moreBtn.style.top = (parseInt(gameCanvas.style.marginTop) || 0) + parseInt(gameCanvas.style.height) - 110 + 'px';
     },
     shouldGameover: function() {
         //if status is 0, the game already stopped,prevent the gameOver callback to execute
